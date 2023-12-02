9a6b0ed5fc78e1bbf08f6f2e4bbbb7f353078bfd
Nargiz <abdullin.nargiz@yandex.ru>
2023-12-02 17:47:15 +0300

    added classes Words,Player

---
+++ src/main/java/com/example/project/GameLogic/Player.java
@@ -0,0 +1,34 @@
+package com.example.project.GameLogic;
+
+public class Player {
+    private String name;
+    private int currentLevel;
+    private int guessedWordCount;
+
+    public Player(String name){
+        this.name = name;
+        this.currentLevel = 1;
+        this.guessedWordCount = 0;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGuessedWordCount() {
+        return guessedWordCount;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+        this.guessedWordCount = 0;
+    }
+    public void incrementGuessedWordsCount() {
+        this.guessedWordCount++;
+    }
+
+}
